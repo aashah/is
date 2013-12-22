07b5f4cb77a20800c4fc33a49b9f5b378ac48d49
Aakash Shah <ashah@mines.edu>
2013-12-22 14:13:13 -0700

    Initial attempt at representing the various ways to build a module

--- buildCmd.go
+++ buildCmd.go
@@ -11,3 +11,53 @@ import (
 func init() {
     fmt.Println("Loading build helper file")
 }
+
+type findTarget func() string
+
+type buildCmd struct {
+    name string
+    cmd string // name of executable
+    build string // command to run on how to build
+    target string // final location of jar
+}
+
+type buildType struct {
+    name string
+    files []string // file(s) that must exist to be considered part of this build
+    getTarget findTarget
+}
+
+var buildList = []*buildCmd{
+    
+}
+
+var buildTypes = []*buildType{
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
+
+func (b *buildCmd) build(dir string) {
+
+}
+
+func getBuildInfo(dir string) *buildType {
+
+}
+
+func getBuildCmd(type string) *buildCmd {
+
+}
