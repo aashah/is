8d99ea323478ae6e9a8e206aea999526d3db5177
Aakash Shah <ashah@mines.edu>
2013-12-22 18:33:02 -0700

    Unmarshalled & validation of maven pom file
    - uses default in case of data not in pom
    - proper errors in the event it cannot build a target path

--- buildMaven.go
+++ buildMaven.go
@@ -1,5 +1,8 @@
 package main
 
+import "fmt"
+import "encoding/xml"
+
 var mavenDependencies = map[string]string{
     "pom": "pom.xml",
 }
@@ -11,19 +14,54 @@ var mavenBuildCmd = &buildCmd{
 
 var mavenBuildType = &buildType{
     name: "Maven",
-    files: []string{
-        "pom.xml",
-    },
+    files: mavenDependencies,
     getTarget: findMavenTarget,
 }
 
-func findMavenTarget(matches[] string) string {
-    // TODO load pom file to find output directory
-    // Target dir:
-    // - project.build.outputDirectory
-    // - Defaults => {project.baseDir}/target
-    // Target name:
-    // - project.build.finalName
-    // - Defaults => {project.artifaceId}-{project.version}
-    return "MAVEN TARGET"
+func findMavenTarget(matches map[string]string) (target string, err error) {
+    fmt.Println("matches:", matches)
+
+    var mavenProject *xmlMavenProject
+    var raw []byte
+
+    if raw, err = readFile(matches["pom"]); err == nil {
+        err = xml.Unmarshal(raw, &mavenProject)
+    }
+
+    var OutDirectory string
+    var FinalName string
+
+    switch {
+    case mavenProject.Build.OutputDirectory == "":
+        OutDirectory = "target"
+    case mavenProject.Build.FinalName == "":
+        switch {
+        case mavenProject.ArtifactId == "":
+            return "", errors.New("Resorting to {project.artifactId for target name, but element not found or set")
+        case mavenProject.Version == "":
+            return "", errors.New("Resorting to {project.version for target name, but element not found or set")
+        }
+        FinalName = fmt.Sprintf("%s-%s", mavenProject.ArtifactId, mavenProject.Version)
+    default:
+        OutDirectory = mavenProject.Build.OutputDirectory
+        Finalname = mavenProject.Build.Finalname
+    }
+    return "MAVEN TARGET", nil  
+}
+
+type xmlMavenProject struct {
+    XMLName xml.Name `xml:"project"`
+    ArtifactId string `xml:"artifactId"`
+    Version string `xml:"version"`
+    Build xmlMavenBuild `xml:"build"`
+}
+
+type xmlMavenBuild struct {
+    OutputDirectory string `xml:"outputDirectory"`
+    FinalName string `xml:"finalName"`
+}
+
+func (x *xmlMavenProject) print() {
+    fmt.Println("Out dir:", x.Build.OutputDirectory)
+    fmt.Println("Final name:", x.Build.FinalName)
 }
